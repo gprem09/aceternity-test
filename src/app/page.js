9a51c0dd567a211decfaaf9b274bd5029ca32f4c
gpa21 <gpa21@sfu.ca>
2024-05-28 18:56:10 -0700

    set up my first hero page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+import Hero from '../components/hero';
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Welcome to My Website</h1>
-      <p className="mt-4 text-lg">My name is Gnanavel Premnath.</p>
-    </main>
-  );
+    return (
+      <div>
+        <Hero />
+      </div>
+    );
 }
