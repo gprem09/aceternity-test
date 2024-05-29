e4e5af62805529da994e76b7d7334f517a4c0914
gpa21 <gpa21@sfu.ca>
2024-05-28 19:21:01 -0700

    update on navbar

--- src/components/hero.js
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <h1 className="text-4xl font-bold">Welcome to My Website</h1>
-            <p className="mt-4 text-lg">Gnanavel Premnath.</p>
-        </main>
-    );
-}
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { IoMdMail } from "react-icons/io";
+import { IoMdMail } from 'react-icons/io';
 
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center bg-transparent p-4">
-            <div className="flex items-center">
-                <a href="#" className="text-white mx-4">Gnanavel Premnath</a>
-                <a href="#" className="text-white mx-4">Home</a>
-                <a href="#" className="text-white mx-4">About Me</a>
-                <a href="#" className="text-white mx-4">Experience</a>
-                <a href="#" className="text-white mx-4">Project</a>
+            <div className="flex items-center ml-52">
+                <a href="#" className="text-white mx-5">Gnanavel Premnath</a>
+                <a href="#" className="text-white mx-5">Home</a>
+                <a href="#" className="text-white mx-5">About Me</a>
+                <a href="#" className="text-white mx-5">Experience</a>
+                <a href="#" className="text-white mx-5">Project</a>
             </div>
-            <div className="flex items-center">
-                <a href="gpa21@sfu.ca" className="text-white mx-2">
-                    <IoMdMail className="w-4 h-4" />
+            <div className="flex items-center mr-52">
+                <a href="mailto:gpa21@sfu.ca" className="text-white mx-2">
+                    <IoMdMail className="w-5 h-5" />
                 </a>
                 <a href="https://github.com/gprem09" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
-                    <FaGithub className="w-4 h-4" />
+                    <FaGithub className="w-5 h-5" />
                 </a>
                 <a href="https://www.linkedin.com/in/gnanavel-premnath-b27a53235/" className="text-white mx-2" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin className="w-4 h-4" />
+                    <FaLinkedin className="w-5 h-5" />
                 </a>
             </div>
         </nav>
